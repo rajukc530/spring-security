89610b3d9bbe03a467e2db910d5a222d8fb9e9c8
raju kc <rajukc530@gmail.com>
2020-11-17 22:32:29 +0545

    -add default role.

--- src/main/java/np/com/xref/AuthAppApplication.java
+++ src/main/java/np/com/xref/AuthAppApplication.java
@@ -1,12 +1,35 @@
 package np.com.xref;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import np.com.xref.model.Role;
+import np.com.xref.model.RoleName;
+import np.com.xref.repository.RoleRepository;
+
 @SpringBootApplication
-public class AuthAppApplication {
+public class AuthAppApplication implements CommandLineRunner {
+
+	@Autowired
+	RoleRepository role;
 
 	public static void main(String[] args) {
 		SpringApplication.run(AuthAppApplication.class, args);
 	}
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+			Role adminRole = new Role();
+			adminRole.setName(RoleName.ROLE_ADMIN);
+			role.save(adminRole);
+			
+			Role userRole = new Role();
+			userRole.setName(RoleName.ROLE_USER);
+			role.save(userRole);
+
+	}
+		
 }
